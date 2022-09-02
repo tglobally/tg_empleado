2f7df4f9e55975f906976bb0396fc405d192c820
MauricioHdz <mauricioisraelhernandez@gmail.com>
2022-09-02 14:37:13 -0500

    v0.13.2 Se integra secciones alta

--- templates/em_empleado/alta/secciones.php
+++ templates/em_empleado/alta/secciones.php
@@ -9,11 +9,11 @@
             <h3>Alta Empleado</h3>
             <?php include "templates/em_empleado/_base/buttons/1.azul.alta.php"; ?>
             <hr class="hr-menu-lateral">
-            <?php include "templates/em_empleado/_base/buttons/2.azul.fiscales.php"; ?>
+            <?php include "templates/em_empleado/_base/buttons/2.gris.fiscales.php"; ?>
             <hr class="hr-menu-lateral">
-            <?php include "templates/em_empleado/_base/buttons/3.azul.imss.php"; ?>
+            <?php include "templates/em_empleado/_base/buttons/3.gris.imss.php"; ?>
             <hr class="hr-menu-lateral">
-            <?php include "templates/em_empleado/_base/buttons/4.azul.cuenta_bancaria.php"; ?>
+            <?php include "templates/em_empleado/_base/buttons/4.gris.cuenta_bancaria.php"; ?>
         </div>
     </div>
 </div>
