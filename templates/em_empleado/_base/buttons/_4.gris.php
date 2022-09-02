68479763e67207a49ed6aeb9484f2973df471573
MauricioHdz <mauricioisraelhernandez@gmail.com>
2022-09-02 13:53:12 -0500

    v0.12.2 Se integra secciones cuenta_bancaria

--- templates/em_empleado/_base/buttons/_4.gris.php
+++ templates/em_empleado/_base/buttons/_4.gris.php
@@ -1,2 +1,2 @@
-<?php include "templates/tg_cte_tipo_alianza/_base/numero/4.gris.php"; ?>
-<?php include "templates/tg_cte_tipo_alianza/_base/texto_menu_lateral/contacto.php"; ?>
+<?php include "templates/em_empleado/_base/numero/4.gris.php"; ?>
+<?php include "templates/em_empleado/_base/texto_menu_lateral/cuenta_bancaria.php"; ?>
--- templates/em_empleado/_base/links/cuenta_bancaria.php
+++ templates/em_empleado/_base/links/cuenta_bancaria.php
@@ -1,4 +1,4 @@
 <?php /** @var \tglobally\tg_empleado\controllers\controlador_em_empleado $controlador */ ?>
-<a href="index.php?seccion=em_cuenta_bancaria&accion=alta&session_id=<?php echo $controlador->session_id;?>">
-    <?php include "templates/em_empleado/_base/buttons/2.gris.cuenta_bancaria.php"; ?>
+<a href="index.php?seccion=em_empleado&accion=cuenta_bancaria&session_id=<?php echo $controlador->session_id;?>&registro_id=<?php echo $controlador->registro_id; ?>">
+    <?php include "templates/em_empleado/_base/buttons/4.gris.cuenta_bancaria.php"; ?>
 </a>
