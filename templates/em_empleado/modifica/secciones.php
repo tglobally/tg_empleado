c60257e48e99a6bede8c8300abba98002752f8ae
MauricioHdz <mauricioisraelhernandez@gmail.com>
2022-09-02 12:56:08 -0500

    v0.9.2 Se integra ajuste en enlace secciones modifica

--- templates/em_empleado/modifica/secciones.php
+++ templates/em_empleado/modifica/secciones.php
@@ -6,7 +6,7 @@
             <img src="<?php echo (new views())->url_assets.'img/stepper/1.svg'?>" class="img-seccion">
         </div>
         <div class="col-md-8">
-            <h3>Alta Empleado</h3>
+            <h3>Modifica Empleado</h3>
             <?php include "templates/em_empleado/_base/buttons/1.azul.modifica.php"; ?>
             <hr class="hr-menu-lateral">
             <?php include "templates/em_empleado/_base/links/fiscales.php"; ?>
