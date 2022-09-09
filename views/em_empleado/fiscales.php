07c161d2798f4affc1a4be6864a8ee460f7141e8
Kevin <kevin.acuna@ivitec.com.mx>
2022-09-09 17:53:14 -0500

    v.0.16.3 Se ajusta link en accion de modifica empleado

--- views/em_empleado/fiscales.php
+++ views/em_empleado/fiscales.php
@@ -6,7 +6,7 @@
         <h3 class="text-center titulo-form">Hola, <?php echo $controlador->datos_session_usuario['adm_usuario_user']; ?> </h3>
 
         <div class="  form-main" id="form">
-            <form method="post" action="./index.php?seccion=em_empleado&accion=fiscales&session_id=<?php echo $controlador->session_id; ?>&registro_id=<?php echo $controlador->registro_id; ?>" class="form-additional">
+            <form method="post" action="./index.php?seccion=em_empleado&accion=modifica_bd&session_id=<?php echo $controlador->session_id; ?>&registro_id=<?php echo $controlador->registro_id; ?>" class="form-additional">
                 <?php echo $controlador->inputs->select->cat_sat_regimen_fiscal_id; ?>
                 <?php echo $controlador->inputs->rfc; ?>
 
