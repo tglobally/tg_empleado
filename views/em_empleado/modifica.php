9b9dd7d1b5c842cdd87d355fe7fa8fa27bfec2e3
[user] <angel.martinez@ivitec.com.mx>
2022-09-09 15:41:16 -0500

    v0.15.3 Se arregla funcion modifica empleado

--- views/em_empleado/modifica.php
+++ views/em_empleado/modifica.php
@@ -6,7 +6,7 @@
         <h3 class="text-center titulo-form">Hola, <?php echo $controlador->datos_session_usuario['adm_usuario_user']; ?> </h3>
 
         <div class="  form-main" id="form">
-            <form method="post" action="./index.php?seccion=em_empleado&accion=modifica_bd&session_id=<?php echo $controlador->session_id; ?>&resgistro_id=<?php echo $controlador->registro_id; ?>" class="form-additional">
+            <form method="post" action="./index.php?seccion=em_empleado&accion=modifica_bd&session_id=<?php echo $controlador->session_id; ?>&registro_id=<?php echo $controlador->registro_id; ?>" class="form-additional">
 
                 <?php echo $controlador->inputs->id; ?>
                 <?php echo $controlador->inputs->codigo; ?>
@@ -22,7 +22,7 @@
 
                 <div class="buttons col-md-12">
                     <div class="col-md-6">
-                        <button type="submit" class="btn btn-info btn-guarda col-md-12 " name="btn_action_next" value="modifica">Guarda</button>
+                        <button type="submit" class="btn btn-info btn-guarda col-md-12 " value="modifica">Guarda</button>
                     </div>
                     <div class="col-md-6 ">
                         <a href="index.php?seccion=em_empleado&accion=fiscales&session_id=<?php echo $controlador->session_id; ?>&resgistro_id=<?php echo $controlador->registro_id; ?>"  class="btn btn-info btn-guarda col-md-12 ">Siguiente</a>
