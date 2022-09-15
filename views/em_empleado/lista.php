309b0c8c99970a6c18da6b9670e143fc4c64baac
Kevin <kevin.acuna@ivitec.com.mx>
2022-09-15 10:34:58 -0500

    v.0.19.4 Se integra cambios en visualizacion de listas de empleados

--- views/em_empleado/lista.php
+++ views/em_empleado/lista.php
@@ -11,60 +11,39 @@ $url_icons = (new views())->url_icons;
     <div class="col-lg-12 content">
         <h3 class="text-center titulo-form">Hola, <?php echo $controlador->datos_session_usuario['adm_usuario_user']; ?></h3>
 
-        <div class="col-md-12 mt-3 table-responsive-sm">
+        <div class="lista">
+            <div class="card">
 
-            <div class="filters">
-                <div class="filter col-md-4 acciones_filter">
-                    <a class="icon_xls_lista">
-                        <img src="<?php echo $url_icons; ?>icon_xls.png">
-                    </a>
-                    <a class="icon_atras_lista">
-                        <img src="<?php echo $url_icons; ?>icon_pag_atras.svg">
-                    </a>
-                    <p class="paginador">3 de 35</p>
-                    <a class="icon_adelante_lista">
-                        <img src="<?php echo $url_icons; ?>icon_pag_adelante.svg">
-                    </a>
-                </div>
-                <div class="search col-md-8 input_search">
-                    <input type="text form-control input">
-                    <img class="input_icon" src="<?php echo $url_icons; ?>icon_lupa.svg">
+                <div class="card-body">
+                    <div class="cont_tabla_sucursal  col-md-12">
+                        <table class="table ">
+                            <thead>
+                            <tr>
+                                <th data-breakpoints="xs sm md" data-type="html" >Id</th>
+                                <th data-breakpoints="xs sm md" data-type="html" >Codigo</th>
+                                <th data-breakpoints="xs sm md" data-type="html" >Empleado</th>
+                                <th data-breakpoints="xs sm md" data-type="html" >Rfc</th>
+                                <th data-breakpoints="xs md" class="control"  data-type="html" data-filterable="false">Modifica</th>
+                                <th data-breakpoints="xs md" class="control"  data-type="html" data-filterable="false">Elimina</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <?php foreach ($controlador->registros as $registro){?>
+                                <tr>
+                                    <td><?php echo $registro->em_empleado_id; ?></td>
+                                    <td><?php echo $registro->em_empleado_codigo; ?></td>
+                                    <td><?php echo $registro->em_empleado_nombre.' '.$registro->em_empleado_ap.' '.$registro->em_empleado_am; ?></td>
+                                    <td><?php echo $registro->em_empleado_rfc; ?></td>
+                                    <td><a class="btn btn-warning " href="<?php echo $registro->link_modifica; ?>">Modifica</a></td>
+                                    <td><a class="btn btn-danger " href="<?php echo $registro->link_elimina; ?>">Modifica</a></td>
+                                </tr>
+                            <?php } ?>
+
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
-
-            <table class="table table-dark">
-                <thead>
-                <tr>
-                    <th scope="col">Acciones</th>
-                    <th scope="col">Id</th>
-                    <th scope="col">Codigo</th>
-                    <th scope="col">Cod Bis</th>
-                    <th scope="col">Descripcion</th>
-                </tr>
-                </thead>
-                <tbody>
-                <?php foreach ($controlador->registros as $registro){
-
-                    ?>
-                    <tr>
-                        <th class="colum_accion" scope="row">
-                            <a class="icon_modifica_lista" href="<?php echo $registro->link_modifica; ?>">
-                                <img src="<?php echo $url_icons; ?>icon_modifica.svg">
-                            </a>
-                            <a class="icon_elimina_lista" href="<?php echo $registro->link_elimina_bd; ?>">
-                                <img src="<?php echo $url_icons; ?>icon_elimina.svg">
-                            </a>
-
-                        </th>
-                        <th><?php echo $registro->em_empleado_id; ?></th>
-                        <th><?php echo $registro->em_empleado_codigo; ?></th>
-                        <th><?php echo $registro->em_empleado_codigo_bis; ?></th>
-                        <th><?php echo $registro->em_empleado_descripcion; ?></th>
-                    </tr>
-                <?php } ?>
-
-                </tbody>
-            </table>
         </div>
 
     </div>
