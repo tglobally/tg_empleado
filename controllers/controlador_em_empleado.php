2c78e633f35bf8dbad7df73eea4e8e168debfc13
MauricioHdz <mauricioisraelhernandez@gmail.com>
2022-09-22 14:25:11 -0500

    v0.23.5 FIX se integra calculo en empleado base

--- controllers/controlador_em_empleado.php
+++ controllers/controlador_em_empleado.php
@@ -23,36 +23,4 @@ class controlador_em_empleado extends \gamboamartin\empleado\controllers\control
 
         $this->titulo_lista = 'Empleados';
     }
-
-    public function calcula_sdi(bool $header, bool $ws = true){
-        $em_empleado_id = $_GET['em_empleado_id'];
-        $fecha_inicio_rel = $_GET['fecha_inicio_rel_laboral'];
-        $salario_diario = $_GET['salario_diario'];
-
-        $result = (new em_empleado($this->link))->calcula_sdi(em_empleado_id: $em_empleado_id,
-            fecha_inicio_rel: $fecha_inicio_rel, salario_diario: $salario_diario);
-        if(errores::$error){
-            return $this->retorno_error(mensaje: 'Error al obtener',data:  $result, header: $header,ws:$ws);
-        }
-
-        if($header){
-            $retorno = $_SERVER['HTTP_REFERER'];
-            header('Location:'.$retorno);
-            exit;
-        }
-        if($ws){
-            header('Content-Type: application/json');
-            try {
-                echo json_encode($result, JSON_THROW_ON_ERROR);
-            }
-            catch (Throwable $e){
-                return $this->retorno_error(mensaje: 'Error al maquetar estados',data:  $e, header: false,ws:$ws);
-            }
-            exit;
-        }
-
-        return $result;
-    }
-
-
 }
