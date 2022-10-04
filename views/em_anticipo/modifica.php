9f98843711994fe846eb25e08e7107c5a6f4e5f6
[user] <angel.martinez@ivitec.com.mx>
2022-10-04 17:53:10 -0500

    v0.46.8 Se ajustan selects en seccion anticipos

--- views/em_anticipo/modifica.php
+++ views/em_anticipo/modifica.php
@@ -8,8 +8,8 @@
 
         <div class="  form-main" id="form">
             <form method="post" action="./index.php?seccion=em_anticipo&accion=modifica_bd&session_id=<?php echo $controlador->session_id; ?>&registro_id=<?php echo $controlador->registro_id; ?>" class="form-additional">
-                <?php echo $controlador->inputs->select->em_empleado_id; ?>
-                <?php echo $controlador->inputs->select->em_tipo_anticipo_id; ?>
+                <?php echo $controlador->inputs->em_empleado_id; ?>
+                <?php echo $controlador->inputs->em_tipo_anticipo_id; ?>
                 <?php echo $controlador->inputs->descripcion; ?>
                 <?php echo $controlador->inputs->monto; ?>
                 <?php echo $controlador->inputs->fecha_prestacion; ?>
