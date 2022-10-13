2d4c641aa6cc277828692cc6850eb14f78b5f2ce
Ivan Avelar <ivan_avelar6@hotmail.com>
2022-10-13 12:08:25 -0500

    v0.51.8 se integra campo CURP

--- controllers/controlador_em_empleado.php
+++ controllers/controlador_em_empleado.php
@@ -26,6 +26,7 @@ class controlador_em_empleado extends \gamboamartin\empleado\controllers\control
         $this->asignar_propiedad(identificador: 'cat_sat_regimen_fiscal_id', propiedades: ['cols'=> 8]);
         $this->asignar_propiedad(identificador: 'im_registro_patronal_id', propiedades: ['cols'=> 12]);
         $this->asignar_propiedad(identificador: 'fecha_inicio_rel_laboral', propiedades: ['cols'=> 8]);
+        $this->asignar_propiedad(identificador: 'curp', propiedades: ['cols'=> 5]);
         $this->asignar_propiedad(identificador: 'codigo', propiedades: ['place_holder'=> 'Codigo']);
         $this->asignar_propiedad(identificador: 'nombre', propiedades: ['place_holder'=> 'Nombre']);
         $this->asignar_propiedad(identificador: 'ap', propiedades: ['place_holder'=> 'Apellido Paterno']);
@@ -33,6 +34,7 @@ class controlador_em_empleado extends \gamboamartin\empleado\controllers\control
         $this->asignar_propiedad(identificador: 'telefono', propiedades: ['place_holder'=> 'Telefono']);
         $this->asignar_propiedad(identificador: 'rfc', propiedades: ['place_holder'=> 'RFC']);
         $this->asignar_propiedad(identificador: 'nss', propiedades: ['place_holder'=> 'NSS']);
+        $this->asignar_propiedad(identificador: 'curp', propiedades: ['place_holder'=> 'CURP']);
         $this->asignar_propiedad(identificador: 'fecha_inicio_rel_laboral',
             propiedades: ['place_holder'=> 'Fecha Inicio Relacion Laboral']);
         $this->controlador_em_anticipo->asignar_propiedad(identificador: 'monto',
