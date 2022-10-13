82365c4d6e72462690a2a9aed426d7183455b3ac
Ivan Avelar <ivan_avelar6@hotmail.com>
2022-10-13 11:54:16 -0500

    v0.50.8 Se ajustan labels de formulario empleado

--- controllers/controlador_em_empleado.php
+++ controllers/controlador_em_empleado.php
@@ -26,7 +26,18 @@ class controlador_em_empleado extends \gamboamartin\empleado\controllers\control
         $this->asignar_propiedad(identificador: 'cat_sat_regimen_fiscal_id', propiedades: ['cols'=> 8]);
         $this->asignar_propiedad(identificador: 'im_registro_patronal_id', propiedades: ['cols'=> 12]);
         $this->asignar_propiedad(identificador: 'fecha_inicio_rel_laboral', propiedades: ['cols'=> 8]);
-
-
+        $this->asignar_propiedad(identificador: 'codigo', propiedades: ['place_holder'=> 'Codigo']);
+        $this->asignar_propiedad(identificador: 'nombre', propiedades: ['place_holder'=> 'Nombre']);
+        $this->asignar_propiedad(identificador: 'ap', propiedades: ['place_holder'=> 'Apellido Paterno']);
+        $this->asignar_propiedad(identificador: 'am', propiedades: ['place_holder'=> 'Apellido Materno']);
+        $this->asignar_propiedad(identificador: 'telefono', propiedades: ['place_holder'=> 'Telefono']);
+        $this->asignar_propiedad(identificador: 'rfc', propiedades: ['place_holder'=> 'RFC']);
+        $this->asignar_propiedad(identificador: 'nss', propiedades: ['place_holder'=> 'NSS']);
+        $this->asignar_propiedad(identificador: 'fecha_inicio_rel_laboral',
+            propiedades: ['place_holder'=> 'Fecha Inicio Relacion Laboral']);
+        $this->controlador_em_anticipo->asignar_propiedad(identificador: 'monto',
+            propiedades: ['place_holder'=> 'Monto']);
+        $this->controlador_em_cuenta_bancaria->asignar_propiedad(identificador: 'clabe',
+            propiedades: ['place_holder'=> 'CLABE']);
     }
 }
