665218911f3b9e795b7ad7e262c6f456424e467c
Kevin <kevin.acuna@ivitec.com.mx>
2022-10-31 14:22:07 -0500

    v0.66.9 Se ajusta views alta y modifica en catalogo empleado

--- js/em_empleado/modifica.js
+++ js/em_empleado/modifica.js
@@ -1,5 +1,3 @@
-let session_id = getParameterByName('session_id');
-
 let sl_dp_pais = $("#dp_pais_id");
 let sl_dp_estado = $("#dp_estado_id");
 let sl_dp_municipio = $("#dp_municipio_id");
@@ -22,123 +20,90 @@ let animaciones = (inputs,efecto = 0,margin = 0) => {
 
 function dp_asigna_estados(dp_pais_id = '',dp_estado_id = ''){
 
-    let sl_dp_estado_id = $("#dp_estado_id");
+    let url = get_url("dp_estado","get_estado", {dp_pais_id: dp_pais_id});
 
-    let url = "index.php?seccion=dp_estado&ws=1&accion=get_estado&dp_pais_id="+dp_pais_id+"&session_id="+session_id;
-
-    $.ajax({
-        type: 'GET',
-        url: url,
-    }).done(function( data ) {  // Función que se ejecuta si todo ha ido bien
-        sl_dp_estado_id.empty();
-        integra_new_option("#dp_estado_id",'Seleccione un estado','-1');
+    get_data(url, function (data) {
+        sl_dp_estado.empty();
+        integra_new_option(sl_dp_estado,'Seleccione un estado','-1');
 
         $.each(data.registros, function( index, dp_estado ) {
-            integra_new_option("#dp_estado_id",dp_estado.dp_estado_descripcion,dp_estado.dp_estado_id,
+            integra_new_option(sl_dp_estado,dp_estado.dp_estado_descripcion,dp_estado.dp_estado_id,
                 "data-dp_estado_predeterminado",dp_estado.dp_estado_predeterminado);
         });
-        sl_dp_estado_id.val(dp_estado_id);
-        sl_dp_estado_id.selectpicker('refresh');
-    }).fail(function (jqXHR, textStatus, errorThrown){ // Función que se ejecuta si algo ha ido mal
-        alert('Error al ejecutar');
-        console.log("The following error occured: "+ textStatus +" "+ errorThrown);
+        sl_dp_estado.val(dp_estado_id);
+        sl_dp_estado.selectpicker('refresh');
     });
-
 }
 
 function dp_asigna_municipios(dp_estado_id = '',dp_municipio_id = ''){
 
-    let sl_dp_municipio_id = $("#dp_municipio_id");
+    let url = get_url("dp_municipio","get_municipio", {dp_estado_id: dp_estado_id});
 
-    let url = "index.php?seccion=dp_municipio&ws=1&accion=get_municipio&dp_estado_id="+dp_estado_id+"&session_id="+session_id;
+    get_data(url, function (data) {
+        sl_dp_municipio.empty();
 
-    $.ajax({
-        type: 'GET',
-        url: url,
-    }).done(function( data ) {  // Función que se ejecuta si todo ha ido bien
-
-        sl_dp_municipio_id.empty();
-
-        integra_new_option("#dp_municipio_id",'Seleccione un municipio','-1');
+        integra_new_option(sl_dp_municipio,'Seleccione un municipio','-1');
 
         $.each(data.registros, function( index, dp_municipio ) {
-            integra_new_option("#dp_municipio_id",dp_municipio.dp_municipio_descripcion,dp_municipio.dp_municipio_id,
+            integra_new_option(sl_dp_municipio,dp_municipio.dp_municipio_descripcion,dp_municipio.dp_municipio_id,
                 "data-dp_municipio_predeterminado",dp_municipio.dp_municipio_predeterminado);
         });
-        sl_dp_municipio_id.val(dp_municipio_id);
-        sl_dp_municipio_id.selectpicker('refresh');
-    }).fail(function (jqXHR, textStatus, errorThrown){ // Función que se ejecuta si algo ha ido mal
-        alert('Error al ejecutar');
-        console.log("The following error occured: "+ textStatus +" "+ errorThrown);
+        sl_dp_municipio.val(dp_municipio_id);
+        sl_dp_municipio.selectpicker('refresh');
     });
-
 }
 
 function dp_asigna_cps(dp_municipio_id = '',dp_cp_id = ''){
 
-    let sl_dp_cp_id = $("#dp_cp_id");
+    let url = get_url("dp_cp","get_cp", {dp_municipio_id: dp_municipio_id});
 
-    let url = "index.php?seccion=dp_cp&ws=1&accion=get_cp&dp_municipio_id="+dp_municipio_id+"&session_id="+session_id;
-    $.ajax({
-        type: 'GET',
-        url: url,
-    }).done(function( data ) {  // Función que se ejecuta si todo ha ido bien
+    get_data(url, function (data) {
+        sl_dp_cp.empty();
 
-        sl_dp_cp_id.empty();
-        integra_new_option("#dp_cp_id",'Seleccione un cp','-1');
+        integra_new_option(sl_dp_cp,'Seleccione un cp','-1');
+
         $.each(data.registros, function( index, dp_cp ) {
-            integra_new_option("#dp_cp_id",dp_cp.dp_cp_descripcion,dp_cp.dp_cp_id,"data-dp_cp_predeterminado",
+            integra_new_option(sl_dp_cp,dp_cp.dp_cp_descripcion,dp_cp.dp_cp_id,"data-dp_cp_predeterminado",
                 dp_cp.dp_cp_predeterminado);
         });
-        sl_dp_cp_id.val(dp_cp_id);
-        sl_dp_cp_id.selectpicker('refresh');
-    }).fail(function (jqXHR, textStatus, errorThrown){ // Función que se ejecuta si algo ha ido mal
-        alert('Error al ejecutar');
+        sl_dp_cp.val(dp_cp_id);
+        sl_dp_cp.selectpicker('refresh');
     });
 }
 
 function dp_asigna_colonias_postales(dp_cp_id = '',dp_colonia_postal_id = ''){
 
-    let sl_dp_colonia_postal_id = $("#dp_colonia_postal_id");
+    let url = get_url("dp_colonia_postal","get_colonia_postal", {dp_cp_id: dp_cp_id});
 
-    let url = "index.php?seccion=dp_colonia_postal&ws=1&accion=get_colonia_postal&dp_cp_id="+dp_cp_id+"&session_id="+session_id;
-    $.ajax({
-        type: 'GET',
-        url: url,
-    }).done(function( data ) {  // Función que se ejecuta si todo ha ido bien
+    get_data(url, function (data) {
+        sl_dp_colonia.empty();
 
-        sl_dp_colonia_postal_id.empty();
-        integra_new_option("#dp_colonia_postal_id",'Seleccione una colonia','-1');
+        integra_new_option(sl_dp_colonia,'Seleccione una colonia','-1');
+
         $.each(data.registros, function( index, dp_colonia_postal ) {
-            integra_new_option("#dp_colonia_postal_id",dp_colonia_postal.dp_cp_descripcion,dp_colonia_postal.dp_colonia_postal_id,
+            integra_new_option(sl_dp_colonia,dp_colonia_postal.dp_colonia_postal_descripcion,dp_colonia_postal.dp_colonia_postal_id,
                 "data-dp_colonia_postal_predeterminado", dp_colonia_postal.dp_colonia_postal_predeterminado);
         });
-        sl_dp_colonia_postal_id.val(dp_colonia_postal_id);
-        sl_dp_colonia_postal_id.selectpicker('refresh');
-    }).fail(function (jqXHR, textStatus, errorThrown){ // Función que se ejecuta si algo ha ido mal
-        alert('Error al ejecutar');
+        sl_dp_colonia.val(dp_colonia_postal_id);
+        sl_dp_colonia.selectpicker('refresh');
     });
 }
 
 function dp_asigna_calles_pertenece(dp_colonia_postal_id = '',dp_calle_pertenece_id = ''){
 
-    let sl_dp_calle_pertenece_id = $("#dp_calle_pertenece_id");
+    let url = get_url("dp_calle_pertenece","get_calle_pertenece", {dp_colonia_postal_id: dp_colonia_postal_id});
 
-    let url = "index.php?seccion=dp_calle_pertenece&ws=1&accion=get_calle_pertenece&dp_colonia_postal_id="+dp_colonia_postal_id+"&session_id="+session_id;
-    $.ajax({
-        type: 'GET',
-        url: url,
-    }).done(function( data ) {  // Función que se ejecuta si todo ha ido bien
-        sl_dp_calle_pertenece_id.empty();
-        integra_new_option("#dp_calle_pertenece_id",'Seleccione una calle','-1');
+    get_data(url, function (data) {
+        sl_dp_calle_pertenece.empty();
+
+        integra_new_option(sl_dp_calle_pertenece,'Seleccione una calle','-1');
+
         $.each(data.registros, function( index, dp_calle_pertenece ) {
-            integra_new_option("#dp_calle_pertenece_id",dp_calle_pertenece.dp_colonia_descripcion,dp_calle_pertenece.dp_calle_pertenece_id,
+            integra_new_option(sl_dp_calle_pertenece,dp_calle_pertenece.dp_calle_pertenece_descripcion,dp_calle_pertenece.dp_calle_pertenece_id,
                 "data-dp_calle_pertenece_predeterminado", dp_calle_pertenece.dp_calle_pertenece_predeterminado);
         });
-        sl_dp_calle_pertenece_id.val(dp_calle_pertenece_id);
-        sl_dp_calle_pertenece_id.selectpicker('refresh');
-    }).fail(function (jqXHR, textStatus, errorThrown){ // Función que se ejecuta si algo ha ido mal
-        alert('Error al ejecutar');
+        sl_dp_calle_pertenece.val(dp_calle_pertenece_id);
+        sl_dp_calle_pertenece.selectpicker('refresh');
     });
 }
 
