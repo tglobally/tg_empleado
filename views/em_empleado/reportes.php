1bfe4aa430e3f84032bd0f8ecd022a7314301d69
Kevin <kevin.acuna@ivitec.com.mx>
2023-01-31 14:30:52 -0500

    v0.83.10 Se ajustan elementros de filtrado

--- views/em_empleado/reportes.php
+++ views/em_empleado/reportes.php
@@ -18,18 +18,38 @@ $url_icons = (new views())->url_icons;
 
                 <div class="card-body">
                     <div class="cont_tabla_sucursal  col-md-12">
-                        <div class="botones" style="display: flex; justify-content: flex-end; align-items: center">
-                            <form method="post" action="<?php echo $controlador->link_em_empleado_exportar; ?> "
-                                  class="form-additional" id="form_export" style="width: 100%">
-                                <?php echo $controlador->inputs->filtro_fecha_inicio; ?>
-                                <?php echo $controlador->inputs->filtro_fecha_final; ?>
-                            </form>
-                            <button type="submit" class="btn btn-info" name="btn_action_next"
-                                    style="border-radius: 5px" value="exportar" form="form_export">
-                                Exportar
-                            </button>
-                        </div>
-                        <table id="em_empleado" class="table table-striped "></table>
+                        <form method="post" action="<?php echo $controlador->link_em_empleado_exportar; ?> "
+                              class="form-additional" id="form_export">
+
+                            <div class="filtros">
+
+                                <div class="filtro-titulo">
+                                    <h3>Estimado usuario, por favor seleccione una opción de busqueda:</h3>
+                                </div>
+
+                                <div class="filtro-categorias">
+                                </div>
+                                <div class="filtro-reportes">
+                                    <div class="filtro-fechas">
+                                        <label>Rango Fechas</label>
+                                        <div class="fechas form-main widget-form-cart">
+                                            <?php echo $controlador->inputs->fecha_inicio; ?>
+                                            <?php echo $controlador->inputs->fecha_final; ?>
+                                        </div>
+                                    </div>
+
+                                </div>
+
+                            </div>
+
+                            <div class="botones">
+                                <button type="submit" class="btn btn-success export" name="btn_action_next"
+                                        style="border-radius: 5px" value="exportar" form="form_export">
+                                    Exportar
+                                </button>
+                            </div>
+                        </form>
+                        <table id="em_empleado" class="datatables table table-striped "></table>
                     </div>
                 </div>
             </div>
