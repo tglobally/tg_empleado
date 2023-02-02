968eb6b45ea801c1abc1e2b22294be816071563c
[user] <angel.martinez@ivitec.com.mx>
2023-02-02 17:45:09 -0600

    v0.90.11 Fix boton reporte empresa

--- templates/em_anticipo/_base/buttons/1.azul.reporte_empresa.php
+++
@@ -1,3 +0,0 @@
-<button class="btn btn-default menu-lateral menu-lateral-active">
-    <?php include "templates/em_anticipo/_base/buttons/_1.azul.reporte_empresa.php"; ?>
-</button>
---
+++ templates/em_anticipo/_base/buttons/2.azul.reporte_empresa.php
@@ -0,0 +1,3 @@
+<button class="btn btn-default menu-lateral menu-lateral-active">
+    <?php include "templates/em_anticipo/_base/buttons/_2.azul.reporte_empresa.php"; ?>
+</button>
