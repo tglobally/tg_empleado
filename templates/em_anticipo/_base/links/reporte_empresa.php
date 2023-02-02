eef9bb4b7ceb835e6cd10780737b209b226edaec
[user] <angel.martinez@ivitec.com.mx>
2023-02-02 17:42:19 -0600

    v0.90.10 Se ajustan secciones catalogo anticipo

---
+++ templates/em_anticipo/_base/links/reporte_empresa.php
@@ -0,0 +1,4 @@
+<?php /** @var \tglobally\tg_empleado\controllers\controlador_em_anticipo $controlador */ ?>
+<a href="index.php?seccion=em_anticipo&accion=reporte_empresa&session_id=<?php echo $controlador->session_id; ?>">
+    <?php include "templates/em_anticipo/_base/buttons/1.azul.reporte_empresa.php"; ?>
+</a>
