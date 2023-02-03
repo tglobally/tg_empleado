b064d2cac6f382f0e9a02faaad7062d1d5d32b49
[user] <angel.martinez@ivitec.com.mx>
2023-02-03 17:29:06 -0600

    v0.91.11 Se ajustan elementos sidebar anticipo

--- controllers/controlador_em_anticipo.php
+++ controllers/controlador_em_anticipo.php
@@ -1,10 +1,7 @@
 <?php
 namespace tglobally\tg_empleado\controllers;
 
-use gamboamartin\errores\errores;
-use html\em_empleado_html;
 use html\org_empresa_html;
-use models\em_empleado;
 use models\org_empresa;
 use PDO;
 use stdClass;
@@ -17,7 +14,46 @@ class controlador_em_anticipo extends \gamboamartin\empleado\controllers\control
     {
         $html_base = new html();
         parent::__construct(link: $link, html: $html_base);
-        $this->titulo_lista = 'Anticipo';
+        $this->titulo_lista = 'Anticipos';
+
+        $this->sidebar['lista']['titulo'] = "Anticipos";
+        $this->sidebar['lista']['menu'] = array(
+            $this->menu_item(menu_item_titulo: "Alta", link: $this->link_alta,menu_seccion_active: true,
+                menu_lateral_active: true),
+            $this->menu_item(menu_item_titulo: "Reporte Empresa", link: $this->link_em_abono_anticipo_reporte_empresa,
+                menu_seccion_active: true));
+
+        $this->sidebar['alta']['titulo'] = "Alta Anticipo";
+        $this->sidebar['alta']['stepper_active'] = true;
+        $this->sidebar['alta']['menu'] = array(
+            $this->menu_item(menu_item_titulo: "Alta", link: $this->link_alta,menu_lateral_active: true),
+            $this->menu_item(menu_item_titulo: "Reporte Empresa", link: $this->link_em_abono_anticipo_reporte_empresa,
+                menu_seccion_active: true));
+
+        $this->sidebar['modifica']['titulo'] = "Modifica Anticipos";
+        $this->sidebar['modifica']['stepper_active'] = true;
+        $this->sidebar['modifica']['menu'] = array(
+            $this->menu_item(menu_item_titulo: "Modifica", link: $this->link_alta,menu_lateral_active: true),
+            $this->menu_item(menu_item_titulo: "Reporte Empresa", link: $this->link_em_abono_anticipo_reporte_empresa,
+                menu_seccion_active: true));
+
+        $this->sidebar['reporte_empresa']['titulo'] = "Reporte Empresa";
+        $this->sidebar['reporte_empresa']['stepper_active'] = true;
+        $this->sidebar['reporte_empresa']['menu'] = array(
+            $this->menu_item(menu_item_titulo: "Alta", link: $this->link_alta,menu_seccion_active: true),
+            $this->menu_item(menu_item_titulo: "Reporte Empresa", link: $this->link_em_abono_anticipo_reporte_empresa,
+        menu_seccion_active: true, menu_lateral_active: true));
+
     }
 
+    public function menu_item(string $menu_item_titulo, string $link, bool $menu_seccion_active = false,bool $menu_lateral_active = false): array
+    {
+        $menu_item = array();
+        $menu_item['menu_item'] = $menu_item_titulo;
+        $menu_item['menu_seccion_active'] = $menu_seccion_active;
+        $menu_item['link'] = $link;
+        $menu_item['menu_lateral_active'] = $menu_lateral_active;
+
+        return $menu_item;
+    }
 }
