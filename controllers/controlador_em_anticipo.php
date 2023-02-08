2b0b88b9d227566b7a32bbd7fa3f88b42298139f
Kevin <kevin.acuna@ivitec.com.mx>
2023-02-08 15:33:40 -0500

    v0.95.14 Se integra accion para exportar reporte de anticipos

--- controllers/controlador_em_anticipo.php
+++ controllers/controlador_em_anticipo.php
@@ -1,15 +1,20 @@
 <?php
 namespace tglobally\tg_empleado\controllers;
 
+use gamboamartin\empleado\models\em_anticipo;
+use gamboamartin\errores\errores;
+use gamboamartin\plugins\exportador;
 use html\org_empresa_html;
+use tglobally\tg_empleado\models\em_empleado;
 use tglobally\tg_empleado\models\org_empresa;
 use PDO;
 use stdClass;
 use tglobally\template_tg\html;
+use tglobally\tg_empleado\models\tg_empleado_sucursal;
 
 class controlador_em_anticipo extends \gamboamartin\empleado\controllers\controlador_em_anticipo {
 
-
+    public string $link_em_anticipo_exportar_cliente = '';
     public function __construct(PDO $link, stdClass $paths_conf = new stdClass())
     {
         $html_base = new html();
@@ -61,6 +66,122 @@ class controlador_em_anticipo extends \gamboamartin\empleado\controllers\control
             $this->menu_item(menu_item_titulo: "Reporte Cliente", link: $this->link_em_anticipo_reporte_cliente,
         menu_seccion_active: true, menu_lateral_active: true));
 
+        $this->link_em_anticipo_exportar_cliente = $this->obj_link->link_con_id(accion: "exportar_cliente",link: $this->link,
+            registro_id: $this->registro_id,seccion: "em_anticipo");
+        if (errores::$error) {
+            $error = $this->errores->error(mensaje: 'Error al obtener link',
+                data: $this->link_em_anticipo_exportar_cliente);
+            print_r($error);
+            exit;
+        }
+
+    }
+
+    public function exportar_cliente(bool $header, bool $ws = false): array|stdClass
+    {
+        $com_sucursal_id = -1;
+        $em_tipo_anticipo_id = -1;
+        $fecha_inicio = date('Y-m-d');
+        $fecha_fin = date('Y-m-d');
+
+        if (isset($_POST['com_sucursal_id'])){
+            $com_sucursal_id = $_POST['com_sucursal_id'];
+        }
+
+        if (isset($_POST['em_tipo_anticipo_id'])){
+            $em_tipo_anticipo_id = $_POST['em_tipo_anticipo_id'];
+        }
+
+        if (isset($_POST['fecha_inicio'])){
+            $fecha_inicio = $_POST['fecha_inicio'];
+        }
+
+        if (isset($_POST['fecha_final'])){
+            $fecha_fin = $_POST['fecha_final'];
+        }
+
+        $filtro['com_sucursal_id'] = $com_sucursal_id;
+        $tg_empleado_sucursal = (new tg_empleado_sucursal($this->link))->filtro_and(filtro: $filtro);
+        if(errores::$error){
+            $error = $this->errores->error(mensaje: 'Error al obtener registros',data:  $tg_empleado_sucursal);
+            print_r($error);
+            die('Error');
+        }
+
+        $data_exportar = array();
+
+        foreach ($tg_empleado_sucursal->registros as $registro){
+            $filtro_anticipo['em_empleado.id'] = $registro['em_empleado_id'];
+            $filtro_anticipo['em_tipo_anticipo.id'] = $em_tipo_anticipo_id;
+            $filtro_especial[0][$fecha_fin]['operador'] = '>=';
+            $filtro_especial[0][$fecha_fin]['valor'] = 'em_anticipo.fecha_prestacion';
+            $filtro_especial[0][$fecha_fin]['comparacion'] = 'AND';
+            $filtro_especial[0][$fecha_fin]['valor_es_campo'] = true;
+
+            $filtro_especial[1][$fecha_inicio]['operador'] = '<=';
+            $filtro_especial[1][$fecha_inicio]['valor'] = 'em_anticipo.fecha_prestacion';
+            $filtro_especial[1][$fecha_inicio]['comparacion'] = 'AND';
+            $filtro_especial[1][$fecha_inicio]['valor_es_campo'] = true;
+
+            $data = (new em_anticipo($this->link))->filtro_and(filtro: $filtro_anticipo, filtro_especial: $filtro_especial);
+            if(errores::$error){
+                $error = $this->errores->error(mensaje: 'Error al obtener registros',data:  $data);
+                print_r($error);
+                die('Error');
+            }
+
+            if ($data->n_registros >0){
+                $data_exportar[] = $data->registros;
+            }
+        }
+
+        $exportador = (new exportador());
+        $registros_xls = array();
+
+        foreach ($data_exportar as $anticipos){
+            foreach ($anticipos as $anticipo){
+                $row = array();
+                $row["nss"] = $anticipo['em_empleado_nss'];
+                $row["codigo_remunerado"] = $anticipo['em_empleado_codigo'];
+                $row["nombre_remunerado"] = $anticipo['em_empleado_nombre'];
+                $row["nombre_remunerado"] .= " ".$anticipo['em_empleado_ap'];
+                $row["nombre_remunerado"] .= " ".$anticipo['em_empleado_am'];
+                $row["razon_social"] = "DEFAULT";
+                $row["concepto"] = $anticipo['em_anticipo_descripcion'];
+                $row["monto_del_anticipo"] = $anticipo['em_anticipo_monto'];
+                $row["tipo_descuento_monto"] = $anticipo['em_tipo_descuento_monto'];
+                $row["sumatoria_de_abonos"] = $anticipo['total_abonado'];
+                $row["saldo"]  = $anticipo['em_anticipo_saldo'];
+                $registros_xls[] = $row;
+            }
+        }
+
+        $keys = array();
+
+        foreach (array_keys($registros_xls[0]) as $key) {
+            $keys[$key] = strtoupper(str_replace('_', ' ', $key));
+        }
+
+        $registros = array();
+
+        foreach ($registros_xls as $row) {
+            $registros[] = array_combine(preg_replace(array_map(function($s){return "/^$s$/";},
+                array_keys($keys)),$keys, array_keys($row)), $row);
+        }
+
+        $resultado = $exportador->listado_base_xls(header: $header, name: $this->seccion, keys:  $keys,
+            path_base: $this->path_base,registros:  $registros,totales:  array());
+        if(errores::$error){
+            $error =  $this->errores->error('Error al generar xls',$resultado);
+            if(!$header){
+                return $error;
+            }
+            print_r($error);
+            die('Error');
+        }
+
+        header('Location:' . $this->link_lista);
+        exit;
     }
 
     public function menu_item(string $menu_item_titulo, string $link, bool $menu_seccion_active = false,bool $menu_lateral_active = false): array
@@ -73,4 +194,6 @@ class controlador_em_anticipo extends \gamboamartin\empleado\controllers\control
 
         return $menu_item;
     }
+
+
 }
--- css/em_anticipo.reporte_cliente.php
+++ css/em_anticipo.reporte_cliente.php
@@ -9,50 +9,50 @@ include $ruta_template_base.'assets/css/_base_css.php';
 
 
     .card{
-        border-radius: 0.5rem !important;
+    border-radius: 0.5rem !important;
         box-shadow: 1px 1px 5px #191919;
     }
 
     .table thead{
-        background-color: #000098;
+    background-color: #000098;
         color: white;
         font-weight: bold;
     }
 
     .dataTables_filter input{
-        border-bottom: 1px solid gray;
+    border-bottom: 1px solid gray;
         box-shadow: 0 4px 3px -3px gray;
         border-radius: 0;
         height: 34px;
     }
 
     .dataTables_filter input:focus{
-        border-bottom: 1px solid gray;
+    border-bottom: 1px solid gray;
         box-shadow: 0 4px 3px -3px gray;
         border-radius: 0;
     }
 
 
     .buttons {
-        margin-bottom: 2.25rem;
+    margin-bottom: 2.25rem;
     }
 
     .lista{
-        margin-top: 20px;
+    margin-top: 20px;
         margin-bottom: 20px;
     }
     .text-header {
-        font-family: Helvetica;
+    font-family: Helvetica;
         font-weight: 700!important;
         color: #000098;
     }
 
     .card {
-        position: relative;
-        display: -webkit-box;
-        display: -ms-flexbox;
-        display: flex;
-        -webkit-box-orient: vertical;
+    position: relative;
+    display: -webkit-box;
+    display: -ms-flexbox;
+    display: flex;
+    -webkit-box-orient: vertical;
         -webkit-box-direction: normal;
         -ms-flex-direction: column;
         flex-direction: column;
@@ -65,44 +65,43 @@ include $ruta_template_base.'assets/css/_base_css.php';
     }
 
     .card-header:first-child {
-        border-radius: calc(0.25rem - 1px) calc(0.25rem - 1px) 0 0;
+    border-radius: calc(0.25rem - 1px) calc(0.25rem - 1px) 0 0;
     }
 
     .card-header {
-        padding: 0.75rem 1.25rem;
+    padding: 0.75rem 1.25rem;
         margin-bottom: 0;
         background-color: rgba(0,0,0,.03);
         border-bottom: 1px solid rgba(0,0,0,.125);
     }
 
     .card-body {
-        -webkit-box-flex: 1;
+    -webkit-box-flex: 1;
         -ms-flex: 1 1 auto;
         flex: 1 1 auto;
         padding: 1.25rem;
     }
 
     .card-title {
-        margin-bottom: 0.75rem;
+    margin-bottom: 0.75rem;
     }
 
 
 
     .footable.table th, .footable-details.table th {
-        font-family: Helvetica;
+    font-family: Helvetica;
     }
     .footable.table td, .footable-details.table td {
-        font-family: Helvetica;
+    font-family: Helvetica;
     }
     .footable .footable-filtering .input-group .form-control:last-child, .footable .footable-filtering .input-group-addon:last-child, .footable .footable-filtering .input-group-btn:last-child > .btn, .footable .footable-filtering .input-group-btn:last-child > .btn-group > .btn, .footable .footable-filtering .input-group-btn:last-child > .dropdown-toggle, .footable .footable-filtering .input-group-btn:first-child > .btn:not(:first-child), .footable .footable-filtering .input-group-btn:first-child > .btn-group:not(:first-child) > .btn {
-        background-color: #0B0595;
+    background-color: #0B0595;
         border: 1px solid #0B0595
     }
 
     .filtros {
         display: grid;
         grid-template-columns: repeat(2, 1fr);
-        grid-template-rows: repeat(2, 1fr);
         grid-gap: 10px;
         grid-auto-rows: minmax(auto, auto);
         margin-bottom: 20px;
@@ -123,7 +122,7 @@ include $ruta_template_base.'assets/css/_base_css.php';
     }
 
     .filtros .filtro-categorias label {
-        font-weight: normal;
+        font-weight: bold;
     }
 
     .filtro-categorias h3 {
@@ -138,7 +137,6 @@ include $ruta_template_base.'assets/css/_base_css.php';
     }
 
 
-
     .botones {
         margin-bottom: 20px;
     }
@@ -147,6 +145,9 @@ include $ruta_template_base.'assets/css/_base_css.php';
         border-radius: 5px;
     }
 
+    .color-secondary{
+        background-color: white !important;
+    }
 
 
 
