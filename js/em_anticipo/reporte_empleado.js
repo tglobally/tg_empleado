71e4eecf857b99da7aaeb5537c44c36a732e8a79
Kevin <kevin.acuna@ivitec.com.mx>
2023-02-28 16:57:51 -0500

    v0.118.16 Se ajusta filtro de busqueda para salida de anticipos por empleado

--- js/em_anticipo/reporte_empleado.js
+++ js/em_anticipo/reporte_empleado.js
@@ -1,38 +1,54 @@
-let sl_com_sucursal = $("#com_sucursal_id");
-
-let url = get_url("em_anticipo", "data_ajax", {em_empleado_id: "1"});
+let url = get_url("em_anticipo", "data_ajax", {em_anticipo_id: "1"});
 
 var datatable = $(".datatables").DataTable({
     processing: true,
     serverSide: true,
     responsive: true,
+    "searching": false,
     ajax: {
         "url": url,
         'data': function (data) {
             var fecha_inicio = $('#fecha_inicio').val();
             var fecha_final = $('#fecha_final').val();
-            //var com_sucursal_id = $( "#com_sucursal_id option:selected" ).val();
+            var em_empleado_id = $('#em_empleado_id').val();
+            var em_tipo_anticipo_id = $('#em_tipo_anticipo_id').val();
 
-            data.filtros = [
-                {
-                    "key": "em_empleado.fecha_inicio_rel_laboral",
-                    "valor": fecha_inicio,
-                    "operador": "<=",
-                    "comparacion": "AND"
-                },
-                {
-                    "key": "em_empleado.fecha_inicio_rel_laboral",
-                    "valor": fecha_final,
-                    "operador": ">=",
-                    "comparacion": "AND"
-                },
-                /*{
-                    "key": "tg_empleado_sucursal.com_sucursal_id",
-                    "valor": com_sucursal_id,
-                    "operador": "=",
-                    "comparacion": "AND"
-                }*/
-            ]
+            data.filtros = {
+                filtro_especial: [
+                    {
+                        "key": "em_anticipo.fecha_prestacion",
+                        "valor": fecha_inicio,
+                        "operador": "<=",
+                        "comparacion": "AND"
+                    },
+                    {
+                        "key": "em_anticipo.fecha_prestacion",
+                        "valor": fecha_final,
+                        "operador": ">=",
+                        "comparacion": "AND"
+                    }
+                ],
+                filtro: []
+            }
+
+            if (em_tipo_anticipo_id !== "") {
+                data.filtros.filtro.push(
+                    {
+                        "key": "em_tipo_anticipo.id",
+                        "valor": em_tipo_anticipo_id
+                    })
+            }
+
+            if (em_empleado_id !== "") {
+                data.filtros.filtro.push(
+                    {
+                        "key": "em_empleado.id",
+                        "valor": em_empleado_id,
+                    }
+                )
+
+
+            }
         },
         "error": function (jqXHR, textStatus, errorThrown) {
             let response = jqXHR.responseText;
@@ -42,40 +58,34 @@ var datatable = $(".datatables").DataTable({
     columns: [
         {
             title: 'Id',
-            data: 'em_empleado_id'
+            data: 'em_anticipo_id'
+        },
+        {
+            title: 'Código',
+            data: 'em_anticipo_codigo'
+        },
+        {
+            title: 'Descripcion',
+            data: 'em_anticipo_descripcion'
         },
         {
             title: 'Empleado',
             data: 'em_empleado_nombre'
         },
         {
-            title: 'Concepto',
-            data: 'em_anticipo_descripcion'
-        },
-        {
             title: 'Monto',
             data: 'em_anticipo_monto'
         },
         {
-            title: 'Fecha Prestación',
+            title: 'Fecha Prestacion',
             data: 'em_anticipo_fecha_prestacion'
         }
     ],
 });
-/*
-$('.filter-checkbox,#fecha_inicio,#fecha_final').on('change', function (e) {
+
+$('.filter-checkbox,#fecha_inicio,#fecha_final,#em_empleado_id,#em_tipo_anticipo_id').on('change', function (e) {
     datatable.draw();
 });
-
-sl_com_sucursal.change(function () {
-    let selected = $(this).find('option:selected');
-
-    if (selected.val() !== ""){
-        datatable.draw();
-    }
-});*/
-
-
 
 
 
