a623444726255a86a98c1b620500563363b85b56
Kevin <kevin.acuna@ivitec.com.mx>
2023-08-22 12:03:19 -0500

    v0.196.17 Se integra columns extra en modelo

--- orm/tg_conf_provisiones_empleado.php
+++ orm/tg_conf_provisiones_empleado.php
@@ -11,9 +11,11 @@ class tg_conf_provisiones_empleado extends _modelo_parent {
         $columnas = array($tabla=>false, 'tg_conf_provision' => $tabla, 'em_empleado' => $tabla,
             'tg_tipo_provision' => $tabla, 'com_sucursal' => 'tg_conf_provision', 'org_sucursal' => 'tg_conf_provision');
         $campos_obligatorios = array();
+        $columnas_extra['em_empleado_nombre_completo'] = 'CONCAT (IFNULL(em_empleado.nombre,"")," ",IFNULL(em_empleado.ap, "")," ",IFNULL(em_empleado.am,""))';
+
 
         parent::__construct(link: $link,tabla:  $tabla, campos_obligatorios: $campos_obligatorios,
-            columnas: $columnas);
+            columnas: $columnas, columnas_extra: $columnas_extra);
 
         $this->NAMESPACE = __NAMESPACE__;
     }
